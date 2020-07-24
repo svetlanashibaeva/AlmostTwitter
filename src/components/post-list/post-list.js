75068a6f88476fc51e93be5a1c37cc979c66ade6
Shibaeva Svetlana <svetlanashibaeva96@gmail.com>
2020-07-24 12:33:35 +0300

    Initial commit

---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ListGroup } from 'reactstrap';
+import PostListItem from '../post-list-item';
+import './post-list.css';
+
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
+
+    const elements = posts.map((item) => {
+        // Простой способ проверки на объект + содержится ли в нем информация
+        if ( typeof item === 'object' && isEmpty(item) ){ 
+            const {id, ...itemProps} = item;
+            return (
+                <li key = {id} className='list-group-item'>
+                    <PostListItem {...itemProps} 
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}/>
+                </li>
+            )
+        }
+    })
+
+    function isEmpty(obj) {
+        for(let key in obj)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    return (
+        <ListGroup className="app-list">
+            {elements}
+        </ListGroup>
+    )
+}
+
+export default PostList;
